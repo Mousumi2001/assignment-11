7130941ae23adae361b9c755bad331452dc51612
Mousumi2001 <am6555264@gmail.com>
2022-10-26 10:59:05 +0600

    create courses layout

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Routers from './component/Router/Routers';
 
 function App() {
   return (
-    <div className="App">
+    <div >
       <Routers></Routers>
     </div>
   );
--- src/component/Courses/Courses.js
+++ src/component/Courses/Courses.js
@@ -2,12 +2,18 @@ import React from 'react';
 
 const Courses = () => {
     return (
-        <div>
-            <div className=''>
-
+        <div className='flex'>
+            <div className='bg-red-300  w-1/3 h-screen'>
+                <h1>HI KDKDJFKDJFKDJFKDJFKDJ</h1>
+                <h1>HI KDKDJFKDJFKDJFKDJFKDJ</h1>
+                <h1>HI KDKDJFKDJFKDJFKDJFKDJ</h1>
+                <h1>HI KDKDJFKDJFKDJFKDJFKDJ</h1>
             </div>
-            <div className=''>
-
+            <div className=' w-2/3'>
+                <h1>HI KDKDJFKDJFKDJFKDJFKDJ</h1>
+                <h1>HI KDKDJFKDJFKDJFKDJFKDJ</h1>
+                <h1>HI KDKDJFKDJFKDJFKDJFKDJ</h1>
+                <h1>HI KDKDJFKDJFKDJFKDJFKDJ</h1>
             </div>
         </div>
     );
