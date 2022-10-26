8f38248925c61dd3b964fa91e42faf546fdd5ae7
Mousumi2001 <am6555264@gmail.com>
2022-10-26 16:08:13 +0600

    create courses card

--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -11,6 +11,7 @@ const Header = () => {
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </label>
                         <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                            <img src="https://tse3.mm.bing.net/th?id=OIP.ylNItzC9JMs3kAtLXuHJMQHaFK&pid=Api&P=0" />
                             <li><Link to='/courses'>Courses</Link></li>
                             <li><Link to='/faq'>FAQ</Link></li>
                             <li><Link to='/blog'>Blog</Link></li>
